8663449b9674450cda83403dcc3c2ff6dbdec7b9
Nika Bostoganashvili <151997372+theBosto@users.noreply.github.com>
2025-01-19 00:43:47 +0400

    UPLOAD

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        spartan: ["League Spartan", "sans-serif"]
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
